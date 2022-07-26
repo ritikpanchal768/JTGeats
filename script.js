ebaf74178375c5e1f7f719fb37fe6d8e5fb631f7
ritikpanchal768 <ritikpanchal768@gmail.com>
2022-07-26 20:29:35 +0530

    carasoul and responsive remaining

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var video = document.querySelector('videos');
+// video functionality
+var video = document.querySelector('video');
 var btn = document.getElementById('play-pause');
 
 function togglePlayPause(){
@@ -15,3 +16,34 @@ function togglePlayPause(){
 btn.onclick = function(){
     togglePlayPause();
 }
+
+// add to cart modal
+var open = document.getElementById('open');
+var modal_container = document.getElementById('modal-container');
+var close = document.getElementById('close');
+
+open.addEventListener('click',()=>{
+    modal_container.classList.add('show');
+});
+
+close.addEventListener('click',()=>{
+    modal_container.classList.remove('show');
+});
+
+// Request a dish Modal
+var opened = document.getElementById('opened');
+var modal_request = document.getElementById('modal-request-a-dish');
+var cancel = document.getElementById('cancel');
+var submit=document.getElementById('submit')
+
+opened.addEventListener('click',()=>{
+    modal_request.classList.add('showed');
+    document.documentElement.scrollTop = 0;
+});
+
+cancel.addEventListener('click',()=>{
+    modal_request.classList.remove('showed');
+});
+submit.addEventListener('click',()=>{
+    modal_request.classList.remove('showed');
+});
